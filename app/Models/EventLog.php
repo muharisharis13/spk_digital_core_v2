ece2951b886979e889e2e5be73f28d7712e7396b
Chrisdiantodoni <95180849+Chrisdiantodoni@users.noreply.github.com>
2024-03-14 14:31:07 +0700

    update

--- app/Models/EventLog.php
+++ app/Models/EventLog.php
@@ -17,4 +17,9 @@ class EventLog extends Model
     {
         return $this->belongsTo(Event::class, "event_id");
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
 }
