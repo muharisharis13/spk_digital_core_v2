92c1f4ee90addb77aeead2208489e58dab61e14f
muharis44 <muharis.haris@frisidea.com>
2024-03-21 20:19:33 +0700

    update

--- app/Http/Middleware/OwnCors.php
+++ app/Http/Middleware/OwnCors.php
@@ -18,8 +18,8 @@ class OwnCors
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', '*');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
     }
