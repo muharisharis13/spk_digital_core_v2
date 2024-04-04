eead6d3bd9e5cb9437587f74d6a228977965371f
muharis44 <muharis.haris@frisidea.com>
2024-04-04 11:17:03 +0700

    update

--- app/Enums/IndentStatusEnum.php
+++ app/Enums/IndentStatusEnum.php
@@ -5,10 +5,10 @@ namespace App\Enums;
 
 class IndentStatusEnum
 {
-    const unpaid = "unpaid_all";
+    const unpaid = "unpaid";
     const cashier_check = "cashier_check";
     const finance_check = "finance_check";
-    const refund = "refund";
+    const refund = "refund_all";
     const cancel = "cancel";
     const spk = "spk";
     const paid = "paid";
