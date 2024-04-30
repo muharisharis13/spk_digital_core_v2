4b2ef05aa96d509a183a59a43816bcfb5c9e3115
muharis44 <muharis.haris@frisidea.com>
2024-05-01 00:25:20 +0700

    update

--- app/Models/ReturUnitLog.php
+++ app/Models/ReturUnitLog.php
@@ -10,6 +10,12 @@ class ReturUnitLog extends Model
 {
     use HasFactory, HasUuids;
     protected $guarded = [];
+    protected $with = ["user"];
 
     protected $primaryKey = "retur_unit_log_id";
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
 }
