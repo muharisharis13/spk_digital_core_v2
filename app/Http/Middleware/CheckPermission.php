98dd4b37483f8115841f6164f811cf3f2de6f9ae
Chrisdiantodoni <95180849+Chrisdiantodoni@users.noreply.github.com>
2024-06-05 00:44:15 +0700

    fixing bug: handling permission

--- app/Http/Middleware/CheckPermission.php
+++ app/Http/Middleware/CheckPermission.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Helpers\ResponseFormatter;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckPermission
@@ -16,8 +17,10 @@ class CheckPermission
      */
     public function handle(Request $request, Closure $next, ...$permissions)
     {
+        Log::info("message", $permissions);
         // Periksa apakah pengguna terotentikasi dan memiliki salah satu permission yang diperlukan
-        if (!$request->user() || !$request->user()->hasAnyPermission($permissions)) {
+        if (!$request->user() || !$request->user()->hasAnyDirectPermission($permissions)) {
+            // if (!$request->user() || !$request->user()->hasAnyPermission($permissions)) {
             // abort(403, 'Unauthorized');
             return ResponseFormatter::error("Kamu tidak memiliki hak akses api ini", "Unauthorized", 403);
         }
