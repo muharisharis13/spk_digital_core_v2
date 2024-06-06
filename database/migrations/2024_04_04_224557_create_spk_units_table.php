55d2d26a1ba00713261de29d25a8bb8cd7ba4827
muharisharis13 <muharisharis13@gmail.com>
2024-06-06 16:31:19 +0700

    update

--- database/migrations/2024_04_04_224557_create_spk_units_table.php
+++ database/migrations/2024_04_04_224557_create_spk_units_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->foreign("spk_id")->references("spk_id")->on("spks")->onDelete("set null");
             $table->uuid("unit_id")->nullable();
             $table->foreign("unit_id")->references("unit_id")->on("unit")->onDelete("set null");
-            $table->string("spk_uniit_year")->nullable();
+            $table->string("spk_unit_year")->nullable();
             $table->timestamps();
         });
     }
