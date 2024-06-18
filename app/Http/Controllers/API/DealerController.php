4ed71867b6e49a2f8f3e7af19b0d0ea83dd2f385
muharis44 <muharis.haris@frisidea.com>
2024-06-18 22:36:11 +0700

    update

--- app/Http/Controllers/API/DealerController.php
+++ app/Http/Controllers/API/DealerController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DealerController extends Controller
 {
     //
+
+    public function createLogoDealer(Request $request)
+    {
+        try {
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return ResponseFormatter::error($e->getMessage(), "Internal Server", 500);
+        }
+    }
 }
