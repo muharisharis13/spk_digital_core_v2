ddb3b6f0f18445578c454d1bc2f19ce23313e27f
muharisharis13 <muharisharis13@gmail.com>
2024-06-19 23:20:03 +0700

    update

---
+++ database/seeders/DeleteAllUserPermissionsAndRolesSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+class DeleteAllUserPermissionsAndRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Hapus semua permissions dan roles dari users
+        User::all()->each(function ($user) {
+            $user->syncPermissions([]);
+            $user->syncRoles([]);
+        });
+
+        // Hapus semua permissions dan roles
+        Permission::query()->delete();
+        Role::query()->delete();
+    }
+}
